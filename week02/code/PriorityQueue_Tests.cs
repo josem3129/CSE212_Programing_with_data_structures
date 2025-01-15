085461f67d8b17a8240d9f064ca4d1dd08d30ce1
josem3129 <mar22069@byui.edu>
2025-01-14 20:18:26 -0700

    week 2 code

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,48 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with the following items and priorities: Item1 (1), Item2 (2), Item3 (3) and find the item with the highest priority
+    // Expected Result: Item3
+    // Defect(s) Found: For loop was not iterating through the entire queue. Changed the for loop to iterate through the entire queue.
     public void TestPriorityQueue_1()
     {
+
+        var item1 = new PriorityItem("Item1", 1);
+        var item2 = new PriorityItem("Item2", 5);
+        var item3 = new PriorityItem("Item3", 3);
+
+        var expectedItem = "Item2";
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(item1.Value, item1.Priority);
+        priorityQueue.Enqueue(item2.Value, item2.Priority);
+        priorityQueue.Enqueue(item3.Value, item3.Priority);
+        
+        var priorityItem = priorityQueue.Dequeue();
+
+        Assert.AreEqual(expectedItem, priorityItem);
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Create a queue with the following items and priorities: Item1 (1), Item2 (3), Item3 (3) and find the item with the highest priority from repeated priorities
+    // Expected Result: Item2
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
+        var item1 = new PriorityItem("Item1", 1);
+        var item2 = new PriorityItem("Item2", 3);
+        var item3 = new PriorityItem("Item3", 3);
+
+        var expectedItem = "Item2";
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue(item1.Value, item1.Priority);
+        priorityQueue.Enqueue(item2.Value, item2.Priority);
+        priorityQueue.Enqueue(item3.Value, item3.Priority);
+        
+        var priorityItem = priorityQueue.Dequeue();
+
+        Assert.AreEqual(expectedItem, priorityItem);
     }
 
     // Add more test cases as needed below.
